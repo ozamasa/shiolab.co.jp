a48042aa08adc840fce95c5bcd090aff35a74f68
Masaki Ozawa <ozamasa@shiolab.com>
2026-02-12 15:01:38 +0900

    fix microcms-js-sdk

--- src/lib/microcms.js
+++ src/lib/microcms.js
@@ -1,6 +1,1 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: import.meta.env.PUBLIC_MICROCMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.PUBLIC_MICROCMS_API_KEY
-});
+// Deprecated: use src/lib/content.js

66feacbad639bd8b47d185813e51b2be46ef5430
Masaki Ozawa <ozamasa@shiolab.com>
2026-02-13 16:10:44 +0900

    update

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -1,19 +1,19 @@
-// src/lib/microcms.ts
 import { createClient } from "microcms-js-sdk";
 
-const serviceDomain = import.meta.env.MICROCMS_SERVICE_DOMAIN;
-const apiKey = import.meta.env.MICROCMS_API_KEY;
-
-export const microcms = (serviceDomain && apiKey)
-  ? createClient({ serviceDomain, apiKey })
-  : null;
+export const microcms =
+  import.meta.env.MICROCMS_SERVICE_DOMAIN &&
+  import.meta.env.MICROCMS_API_KEY
+    ? createClient({
+        serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+        apiKey: import.meta.env.MICROCMS_API_KEY,
+      })
+    : null;
 
 export type Article = {
   id: string;
   title: string;
   description?: string;
-  category?: string;
+  body: string;
+  category?: string | string[] | { name?: string; title?: string } | any;
   publishedAt?: string;
-  createdAt?: string;
-  updatedAt?: string;
 };
